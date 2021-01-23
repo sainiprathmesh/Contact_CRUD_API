9ca400deb48a1401915c7a64cf18b5970a98f9e3
Prathmesh Kumar Saini <prathmesh.saini_cs18@gla.ac.in>
2021-01-23 10:45:28 +0530

    try block activated.

--- helper.py
+++ helper.py
@@ -5,6 +5,11 @@ NOTSTARTED = 'Not Started'
 INPROGRESS = 'In Progress'
 COMPLETED = 'Completed'
 
+
 def add_to_database(record):
     try:
         conn = sqlite3.connect(DB_PATH)
+
+    except Exception as e:
+        print('Error: ', e)
+        return None

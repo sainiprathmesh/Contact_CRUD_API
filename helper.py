4022002717c7672b869e7c39768efe40c8cb30ad
Prathmesh Kumar Saini <prathmesh.saini_cs18@gla.ac.in>
2021-01-23 10:44:45 +0530

    connection for add_to_database() is done.

--- helper.py
+++ helper.py
@@ -1,6 +1,10 @@
 import sqlite3
 
-DB_PATH = './todo.db'
+DB_PATH = './Contact_API.db'
 NOTSTARTED = 'Not Started'
 INPROGRESS = 'In Progress'
 COMPLETED = 'Completed'
+
+def add_to_database(record):
+    try:
+        conn = sqlite3.connect(DB_PATH)

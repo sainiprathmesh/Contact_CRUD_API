7d5b35beffb3a669e7542fecc913d835a1244d4e
Prathmesh Kumar Saini <prathmesh.saini_cs18@gla.ac.in>
2021-01-23 10:56:38 +0530

    function completed create_record_id()

--- helper.py
+++ helper.py
@@ -6,10 +6,24 @@ INPROGRESS = 'In Progress'
 COMPLETED = 'Completed'
 
 
+def create_record_id():
+    rec_id = 'CAPI_'
+    try:
+        conn = sqlite3.connect(DB_PATH)
+        cur = conn.cursor()
+        cur.execute('select * from records')
+        rows = cur.fetchall()
+        rec_id = rec_id + str(len(rows) + 1)
+        return rec_id
+    except Exception as e:
+        print('Error: ', e)
+        return None
+
+
 def add_to_database(record):
     try:
         conn = sqlite3.connect(DB_PATH)
-
+        cur = conn.cursor()
     except Exception as e:
         print('Error: ', e)
         return None
